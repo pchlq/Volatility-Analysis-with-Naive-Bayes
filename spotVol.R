d6e3cb1e38bafb141b17af0cb3efaec0ba2b2e4b
pchlq <peskovps@gmail.com>
2018-09-17 11:28:44 +0300

    replaced the price on returns

--- spotVol.R
+++ spotVol.R
@@ -1,30 +1,33 @@
 library(highfrequency)
 library(rusquant)
 library(dplyr)
+library(TTR)
 
-si <- getSymbols("SPFB.Si", src = "Finam", 
-                 from = "2018-01-03",to = "2018-03-25",
+options(scipen = 10)
+
+ts <- getSymbols("SPFB.Si", src = "Finam", 
+                 from = "2018-06-29",to = "2018-09-14",
                  period = "1min",auto.assign = FALSE)
 
-# split by days
-spl <- si %>% split(f = "days", k=1)
 
-# aggregate each element of list on 5 min
-# NOTE(!): Periodicity estimation requires at least 50 observations (length(arg) > 50)
-agr <- spl %>% lapply(function(x) aggregatePrice(x$SPFB.SI.Close,on = "minutes",k = 5,
-                                                 marketopen = "10:00:00", 
-                                                 marketclose = "23:45:00",
-                                                 tz = "Europe/Moscow"))
+ts.agr <- ts %>% Cl() %>% ROC() %>% na.locf(fromLast = TRUE) %>% # get returns of close price
+              split(f = "days", k=1) %>% # split by days
+              
+  # aggregate each element of list on 5 min
+  # NOTE(!): Periodicity estimation requires at least 50 observations (length(arg) > 50)
+  lapply(function(x) aggregatePrice(x,on = "minutes",k = 5,
+                                    marketopen = "10:00:00", 
+                                    marketclose = "23:45:00",
+                                    tz = "Europe/Moscow"))
+ 
 
-# create a matrix
-mat <- matrix(unlist(agr), nrow = length(agr[[1]]))
+sp <- matrix(unlist(ts.agr), nrow = length(ts.agr[[1]])) %>% # creating a matrix
+  t(.) %>% spotvol(method="detper") # estimating spot volaitility
 
-# estimating spot volaitility
-sp <- t(mat) %>% spotvol(method="detper")
-
+# plot
 plot(sp)
 
 # ==========
 # to check if length of all elements of list agr are equal
-length(unique(lengths(agr))) == 1L
-lapply(agr, function(x) length(x) == length(agr[[1]])) %>% melt(.)
+length(unique(lengths(ts.agr))) == 1L
+lapply(ts.agr, function(x) length(x) == length(ts.agr[[1]])) %>% melt(.)
